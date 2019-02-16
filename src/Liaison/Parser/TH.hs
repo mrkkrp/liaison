fbb862bb182adb0afa535dad31d3619adbc538d7
mrkkrp <markkarpov92@gmail.com>
2019-02-16 18:47:59 +0100

    There gonna be some TH helpers for parsing too

---
+++ src/Liaison/Parser/TH.hs
@@ -0,0 +1,15 @@
+-- |
+-- Module      :  Liaison.Parser.TH
+-- Copyright   :  © 2019 Mark Karpov
+-- License     :  BSD 3 clause
+--
+-- Maintainer  :  Mark Karpov <markkarpov92@gmail.com>
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- TH helpers for parsing at compile time.
+
+module Liaison.Parser.TH
+  (
+  )
+where
